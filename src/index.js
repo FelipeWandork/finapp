faf095705cdd2a2efe14ddbf33e65fb81a221bd9
Felipe Wandork <wandork@gmail.com>
2022-05-11 08:56:13 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const { application } = require('express');
+const { response } = require('express');
 const express           = require('express');
 const { v4: uuidv4 }    = require('uuid');
 const app               = express();
@@ -7,7 +9,7 @@ app.use(express.json());
 
 
 function verifyIfExistsAccountCPF(request, response, next) {
-    const { cpf } = request.headers;
+    const { cpf }  = request.headers;
     const customer = customers.find((customer) => customer.cpf === cpf);
 
     if(!customer) {
@@ -52,7 +54,7 @@ app.post("/account", (request, response) => {
 
 });
 
-// usar o middleware em todas rotas
+// como usar o middleware em todas rotas
 // app.use(verifyIfExistsAccountCPF);
 
 
@@ -96,6 +98,52 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
     customer.statement.push(statementOperation);
 
     return response.status(200).send();
+});
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer }  = request;
+    const { date }      = request.query;
+
+    const dateFormat = new Date(date + " 00:00");
+
+    const statement = customer.statement.filter(
+        (statement) =>
+        statement.created_at.toDateString() ===
+        new Date(dateFormat).toDateString()
+    );
+
+    return response.json(statement);
+});
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
+
 });
 
 app.listen(3333);
